76591e411cbc03ff574ef936de71de3bc879388b
nqerz <73216018+nqerz@users.noreply.github.com>
2024-04-17 14:19:20 +0800

    feat: add kube config backup when creating a new KinD cluster

--- scripts/make-rules/kind.mk
+++ scripts/make-rules/kind.mk
@@ -4,10 +4,14 @@ KIND_CLUSTER_CONTEXT_NAME := $(LOCAL_K8S_TOOL)-$(CLUSTER_NAME)
 .PHONY: kind/create kind/delete kind/list
 
 ##@ Kind Cluster Management
-kind/create: ## Create a kind cluster with istio extra port mappings.
+kind/create: ## Create a kubernetes cluster with istio extra port mappings.
 	@echo "Creating kubernetes cluster name: $(CLUSTER_NAME), config file: $(CLUSTER_CONFIGFILE)"
 	@kind create cluster --name=$(CLUSTER_NAME) --config=$(CLUSTER_CONFIGFILE).yaml
-	kind get kubeconfig --name=$(CLUSTER_NAME) > $(HOME)/.kube/$(LOCAL_K8S_TOOL)-config
+	@if [ -f $(HOME)/.kube/$(LOCAL_K8S_TOOL)-config ]; then \
+		mv $(HOME)/.kube/$(LOCAL_K8S_TOOL)-config $(HOME)/.kube/$(LOCAL_K8S_TOOL)-config.$(CLUSTER_NAME); \
+		echo "Backed up existing kubeconfig to $(HOME)/.kube/$(LOCAL_K8S_TOOL)-config.$(CLUSTER_NAME)"; \
+	fi
+	@kind get kubeconfig --name=$(CLUSTER_NAME) > $(HOME)/.kube/$(LOCAL_K8S_TOOL)-config
 
 kind/delete: ## Delete a kind cluster with the given configs.
 	@echo "Deleting kubernetes cluster name: $(CLUSTER_NAME)"
